f8d7b25518d826b252663c6b564acaca6a8978ee
gwensw <gwen.simswilliams@gmail.com>
2019-01-05 11:05:30 +0000

    Tidy js formatting

--- htdocs/toggle.js
+++ htdocs/toggle.js
@@ -1,18 +1,16 @@
 function toggle(id) {
-
-    if (id.classList.contains("show")) {
-        id.classList.remove("show");
-        id.classList.add("hide");
-    } else if (id.classList.contains("hide")) {
-        id.classList.remove("hide");
-        id.classList.add("show");
-    }
+  if (id.classList.contains('show')) {
+    id.classList.remove('show');
+    id.classList.add('hide');
+  } else if (id.classList.contains('hide')) {
+    id.classList.remove('hide');
+    id.classList.add('show');
+  }
 }
 
 function hideMenu(id) {
-
-    if (id.classList.contains("show")) {
-        id.classList.remove("show");
-        id.classList.add("hide");
-    }
+  if (id.classList.contains('show')) {
+    id.classList.remove('show');
+    id.classList.add('hide');
+  }
 }
